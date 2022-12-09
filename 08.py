cf5123b9d988c12dfa529b11fd904fa51680a29a
Jesper <jdk@magenta-aps.dk>
2022-12-09 09:47:43 +0100

    Day 8

--- 08.py
+++ 08.py
@@ -11,6 +11,15 @@ class Tree:
         self.height = height
         self.visible = False
 
+        self.up_count = 0
+        self.left_count = 0
+        self.down_count = 0
+        self.right_count = 0
+
+    @property
+    def scenic_score(self):
+        return self.up_count * self.left_count * self.down_count * self.right_count
+
     def __str__(self):
         return f"Tree of height {self.height}"
 
@@ -22,7 +31,7 @@ def count_trees(trees):
     return sum([sum([tree.visible for tree in tree_row]) for tree_row in trees])
 
 
-tree_heights = load("data/08/test.txt")
+tree_heights = load("data/08/input1.txt")
 
 # Assignment 1
 
@@ -74,3 +83,36 @@ print("Visible trees: ", count_trees(trees))
 
 # Assignment 2
 
+max_scenic = 0
+
+for row, tree_row in enumerate(trees):
+    for col, tree in enumerate(tree_row):
+        # Check up
+        for y in range(row - 1, -1, -1):
+            tree.up_count += 1
+            if trees[y][col].height >= tree.height:
+                break
+
+        # Check left
+        for x in range(col - 1, -1, -1):
+            tree.left_count += 1
+            if trees[row][x].height >= tree.height:
+                break
+
+        # Check down
+        for y in range(row + 1, len(trees)):
+            tree.down_count += 1
+            if trees[y][col].height >= tree.height:
+                break
+
+        # Check right
+        for x in range(col + 1, len(tree_row)):
+            tree.right_count += 1
+            if trees[row][x].height >= tree.height:
+                break
+
+        if tree.scenic_score > max_scenic:
+            max_scenic = tree.scenic_score
+
+print("Assignment 2:")
+print("Max scenic score: ", max_scenic)
